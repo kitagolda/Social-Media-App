45269a1ebcb790e13553be489f7d7c76fc50cf34
Nikita Golda <kitagolda@gmail.com>
2020-10-29 00:29:50 +0200

    Registration, Login, Comments, Likes added

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
-
-const typeDefs = gql`
-    type Post{
-        id: ID!,
-        body: String!,
-        username: String!,
-        createdAt: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`;
-
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(err){
-                 throw new Error(err);
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => ({ req })
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
